25596963170f4abfc106969c1bab2d9026bb75e3
Ruth27 <ruth27_12@hotmail.com>
2026-02-11 00:10:26 +0100

    prueba ci/cd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PostsListComponent } from './pages/posts-list/posts-list';
+import { PostDetailComponent } from './pages/post-detail/post-detail';
+import { PostFormComponent } from './pages/post-form/post-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: 'posts', component: PostsListComponent },
+  { path: 'posts/new', component: PostFormComponent }, // Aquí
+  { path: 'posts/:id/edit', component: PostFormComponent },
+  { path: 'posts/:id', component: PostDetailComponent },
+  { path: '**', redirectTo: 'posts' },
+];
